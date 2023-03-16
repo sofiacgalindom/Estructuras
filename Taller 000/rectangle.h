f40c3051426bae6e3ba06c24c0d7e3b2bef1d5e6
sofiacgalindom <125517416+sofiacgalindom@users.noreply.github.com>
2023-03-15 19:18:36 -0500

    Add files via upload

---
+++ Taller 000/rectangle.h
@@ -0,0 +1,16 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+struct Rectangle {
+  float posX;
+  float posY;
+  int width;
+  int height;	
+};
+
+float perimeterRect ( Rectangle rect );
+float areaRect ( Rectangle rect );
+float distOriginRect ( Rectangle rect );
+
+
+#endif
